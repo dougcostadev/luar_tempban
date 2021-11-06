b60886479ba3281542932017f2a274e55477fa2b
Douglas Costa <42327794+k7doug@users.noreply.github.com>
2021-11-06 09:33:06 -0300

    Update galaxysun.lua

--- galaxy/galaxysun.lua
+++ galaxy/galaxysun.lua
@@ -1,8 +1,8 @@
 Doug = {
     ComandoTempBan = 'tempban', -- Comando que vai ser dado para banir o player temporariamente.
     PermissaoUsarComando = {'manager.permissao','policia.permissao'}, -- Se quiser adicionar mais permissões para usar o comando, basta seguir o exemplos que já existem.
-    LinkDiscord = "discord.gg/cMMZ9KB6Yn",
-    UsarHoraDia = 'dia',
+    LinkDiscord = "discord.gg/cMMZ9KB6Yn", -- Coloque o link do seu servidor do discord.
+    UsarHoraDia = 'dia', -- Se você deixar "dia", o temp-ban será aplicado em dias, se você usar "hora" o temp-ban será aplicado em horas.
 
-    ComandoRetirarTempBan = 'untempban',
+    ComandoRetirarTempBan = 'untempban', -- Comando que vai ser dado para retirar o banimento temporário.
 }
